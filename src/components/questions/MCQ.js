c5fd87a615fd42ca78891c2d9d2d5c53f537e75a
navyan02 <130892564+navyan02@users.noreply.github.com>
2025-04-05 18:04:13 -0500

    changed color

--- src/components/questions/MCQ.js
+++ src/components/questions/MCQ.js
@@ -3,13 +3,13 @@ import * as React from "react"
 const MCQ = ({ question, options, onSubmit }) => {
   return (
     <div className="flex flex-col pb-2">
-      <h2 className="text-2xl font-bold mb-2">{question}</h2>
+      <h2 className="text-2xl font-bold mb-2 text-black">{question}</h2>
       <ul>
         {options.map(option => {
           return (
             <li key={option}>
               <button
-                className="w-full bg-indigo-100 hover:bg-indigo-200 p-2 border-2 border-indigo-800 rounded-md mb-2 cursor"
+                className="w-full bg-indigo-100 hover:bg-indigo-200 p-2 border-2 border-indigo-800 rounded-md mb-2 text-black"
                 onClick={() => onSubmit(option)}
               >
                 {option}
