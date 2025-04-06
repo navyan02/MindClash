e347e9d202a095bc17be83bc1990879db195f837
navyan02 <130892564+navyan02@users.noreply.github.com>
2025-04-05 21:29:03 -0500

    Update auth-context.js

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,40 +1,25 @@
-import React, { useContext, useState, useEffect } from "react"
+import React, { useContext } from "react"
 import useLocalStorage from "../hooks/useLocalStorage"
 import Login from "../components/login/Login"
-
 export const isBrowser = () => typeof window !== "undefined"
 
 const AuthContext = React.createContext()
 
 export const AuthProvider = ({ ...props }) => {
-  const [user, setUser] = isBrowser() ? useLocalStorage("user", {}) : useState({})
-  const [hasMounted, setHasMounted] = useState(false)
-
-  useEffect(() => {
-    if (isBrowser()) {
-      setHasMounted(true)
-    }
-  }, [])
-
-  if (!hasMounted) {
-    return null 
-  }
+  const [user, setUser] = useLocalStorage("user", {})
 
   const isLoggedIn = !!user.email
 
   if (!isLoggedIn) {
     return <Login setUser={setUser} />
   }
-
   const logout = () => {
-    setUser({})
+      setUser({})
   }
 
   const name = user.name || user.displayName || "Unknown."
 
-  return (
-    <AuthContext.Provider value={{ user, isLoggedIn, logout, name }} {...props} />
-  )
+  return <AuthContext.Provider value={{ user, isLoggedIn, logout, name}} {...props} />
 }
 
 export const useAuth = () => useContext(AuthContext)
